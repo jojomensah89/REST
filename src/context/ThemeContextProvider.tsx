a91993bc7a916fee3bf52ba38203278c41c38028
ebenezermensah <103418784+ebenezermensah@users.noreply.github.com>
2023-01-31 09:46:37 +0000

    added the persistent theme mode

--- src/context/ThemeContextProvider.tsx
+++ src/context/ThemeContextProvider.tsx
@@ -10,7 +10,25 @@ export const ThemeContext = createContext({
 });
 
 export function ThemeProvider({ children }: childrenType) {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+  // Use useEffect to read the theme from local storage
+  useEffect(() => {
+    const localTheme = localStorage.getItem("theme");
+    if (localTheme) {
+      setTheme(localTheme);
+    } else {
+      setTheme("light");
+    }
+  }, []);
+  
+  // Use useEffect to write the theme to local storage
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+    document.body.className = theme;
+  }, [theme]);
+
+
+
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
       {children}
