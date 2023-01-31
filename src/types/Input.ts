69ef74b100012b0892bf06dbd3c0767f781780fc
ebenezermensah <103418784+ebenezermensah@users.noreply.github.com>
2023-01-31 10:53:02 +0000

    Updated the type for the input

--- src/types/Input.ts
+++ src/types/Input.ts
@@ -1,4 +1,7 @@
 export interface InputTs {
-  value: string;
+  searchValue: string;
+  regionValue: string;
   Search: (e: string) => void;
+  Filter: (e: string) => void;
+  // FilterAndSearch: (e: string) => void;
 }
